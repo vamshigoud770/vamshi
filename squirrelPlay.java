32b326ecf62c8787ea5663c3a442bcec536e026e
vamshigoud770 <2311cs010533@mallareddyuniversity.ac.in>
2025-01-06 13:23:39 +0530

    Create squirrelPlay.java

---
+++ squirrelPlay.java
@@ -0,0 +1,7 @@
+public boolean squirrelPlay(int temperature, boolean isSummer) {
+    if (isSummer) {
+        return temperature >= 60 && temperature <= 100;
+    } else {
+        return temperature >= 60 && temperature <= 90;
+    }
+}
