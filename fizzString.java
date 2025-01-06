99630f2504b2c5e1adefa13128ab4cfd67f600ba
vamshigoud770 <2311cs010533@mallareddyuniversity.ac.in>
2025-01-06 17:57:23 +0530

    Create fizzString.java

---
+++ fizzString.java
@@ -0,0 +1,12 @@
+public String fizzString(String str) {
+    if (str.charAt(0) == 'f' && str.charAt(str.length() - 1) == 'b') {
+        return "FizzBuzz";
+    }
+    else if (str.charAt(0) == 'f') {
+        return "Fizz";
+    }
+    else if (str.charAt(str.length() - 1) == 'b') {
+        return "Buzz";
+    }
+    return str;
+}
